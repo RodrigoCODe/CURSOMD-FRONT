0806ea18b9356a12d7caeb228c21d53f6acf6a48
Rodrigo <rodrigo.oscar.alarcon@gmail.com>
2021-12-10 22:32:54 -0300

    password validation complete, fixed issues

--- ejercicioFormulario/script.js
+++ ejercicioFormulario/script.js
@@ -3,6 +3,7 @@ const form = document.querySelector(`.form1`);
 const nombre = document.querySelector(`#nombre`);
 const apellido = document.querySelector('#apellido');
 const email = document.querySelector('#email');
+const password = document.querySelector(`#password`);
 const checkbox = document.querySelector('#terminos');
 const submitBtn = document.querySelector(`.submitBtn`);
 const div = document.querySelector(".div");
@@ -15,15 +16,15 @@ const labelEmail = document.querySelector(`.labelEmail`);
 const labelPassword = document.querySelector(`.labelPassword`);
 
 // Patterns for validation
-const soloLetras = /^([a-zA-Z À-ÿ\u00f1\u00d1\ñ]{2,15})$/;
+const soloLetras = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1\S*]+$/g;
 const regexEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-const regexPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+const regexPass = /^(?=.*\d)(?=.*[a-z\ñ])(?=.*[A-Z\Ñ])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/g;
 
 const patternNum = /[0-9]/;
-const patternChar = /[a-z À-ÿ\u00f1\u00d1\ñ]/;
-const patternUpper= /[A-Z\Ñ]/;
-const patternSize = /^.{8,15}$/;
-const patternSymbol =  /[!@#$%^&*_-]/;
+const patternChar = /[a-z À-ÿ\u00f1]/;
+const patternUpper= /[A-Z À-ÿ \u00d1]/;
+const patternSymbol =  /(?=.*[/¿?¡~+><°;.=!@#\$%\^&\*_-])/;
+const patterWhiteSpaceFinal = /.\S/g;
   
 
 
@@ -78,7 +79,8 @@ function validation(element, label, regexPattern){
     let result = element.addEventListener("input", (event) =>{
         const elementName = event.target.name;
         const value = event.target.value;
-        console.log(elementName);
+        let arrayStringValue = value.split("");
+        // console.log(elementName);
         if(!value){
             resetInput(element, label);
         }else{
@@ -86,11 +88,12 @@ function validation(element, label, regexPattern){
                 // console.log(value);
                 // console.log(!value);
                 console.log("Entra al error: ");
-                console.log(!validatePattern(value, regexPattern));
-                arrayStringValue = value.split("");
+                console.log(!validatePattern(value, regexPattern) && !!value);
+                
                 // console.log(arrayStringValue);
                 showInputError(element);
                 let error = getCorrectLabel(elementName, arrayStringValue);
+                console.log(error);
                 renderLabelError(label, error);
                
             }else{
@@ -101,47 +104,70 @@ function validation(element, label, regexPattern){
             }
         }
     });
+    return result;
 }
 
 
-function getCorrectLabel(elementName, arrayStringValue){ 
-    if(arrayStringValue === undefined){
-        arrayStringValue = "";
+function getCorrectLabel(elementName, arrayStringValue){
+    console.log(elementName);
+    let elementString = elementName.toString();
+    let arrayInternal = new Array();
+    
+    let num= 0;
+    arrayStringValue.forEach(element => {
+        arrayInternal[num] = element;
+        num= num+1;
+        
+    });
+
+    if(arrayInternal === undefined){
+        arrayInternal="";
     }else{
-        switch (elementName){
+        switch (elementString){
             case "nombre":
-                if(!soloLetras.test(arrayStringValue)){
-                    return "El nombre solo puede tener de 2 a 15 letras";
+                if(patterWhiteSpaceFinal.test(arrayInternal)){
+                    return "El nombre no debe terminar en espacio vacio";
                 }
+                if(!soloLetras.test(arrayInternal)){
+                    return "El nombre debe contener de 2 a 15 letras"
+                }
+            
             case "apellido":
-                if(!soloLetras.test(arrayStringValue)){
+                if(patterWhiteSpaceFinal.test(arrayInternal)){
+                    return "El apellido no debe terminar en espacio vacio";
+                }
+                if(!soloLetras.test(arrayInternal)){
                     return "El apellido solo puede tener de 2 a 15 letras";
                 }
             case "email":
-                if(!regexEmail.test(arrayStringValue)){
+                if(!regexEmail.test(arrayInternal)){
                     return "Ingresa una dirección válida";
                 }
             case "password":
-                if(!patternNum.test(arrayStringValue)){
-                    return "La clave debe contener un numero";
-                }
-                if(!patternChar.test(arrayStringValue)){
-                    return "La clave debe contener una letra";
-                }
-    
-                if(!patternUpper.test(arrayStringValue)){
-                    return "La clave debe contener una Mayuscula"
-                }
-                if(!patternSize.test(arrayStringValue)){
-                    return "La clave debe tener 8 caracteres";
-                }
+                
+                    if(!patternNum.test(arrayInternal)){
+                        return "La clave debe contener un numero";
+                    }
+                    else if(!patternChar.test(arrayInternal)){
+                        return "La clave debe contener una letra";
+                    }
+        
+                    else if(!patternUpper.test(arrayInternal)){
+                        return "La clave debe contener una Mayuscula"
+                    }
+                    else if(!patternSymbol.test(arrayInternal)){
+                        return "La clave debe tener al menos 1 caracter especial";
+                    }
+                    else if(arrayInternal.length < 8){
+                        console.log("numero menos de 8");
+                        return "La clave debe tener al menos 8 caracteres";
+                    }
+                    
             default:
                 return "";
         }
     }
 } 
-
-
 
 validation(nombre, labelNombre, soloLetras);
 validation(apellido, labelApellido, soloLetras);
